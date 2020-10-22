6ee570c96624542edb353b721f722e6a6144e2a9
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2020-10-22 19:31:07 +0530

    lassoletter

--- decrypt.py
+++ decrypt.py
@@ -14,3 +14,25 @@ def chant( phrase ):
 
 # Invoke the power chant on the phrase "WONDER WOMAN 1984!"
 chant( "WONDER WOMAN 1984!" )
+
+# Define a function to find the truth by shifting the letter by the specified amount
+def lassoLetter( letter, shiftAmount ):
+    # Invoke the ord function to translate the letter to its ASCII code 
+    # Save the code to the letterCode variable
+    letterCode = ord(letter.lower())
+    
+    # The ASCII number representation of lowercase letter 'a'
+    aAscii = ord('a')
+
+    # The number of letters in the alphabet
+    alphabetSize = 26
+
+    # The formula to calculate the ASCII number for the decoded letter
+    # Take into account looping around the alphabet
+    trueLetterCode = aAscii + (((letterCode - aAscii) + shiftAmount) % alphabetSize)
+
+    # Convert the ASCII number to the character or letter
+    decodedLetter = chr(trueLetterCode)
+
+    # Send the decoded letter back
+    return decodedLetter
