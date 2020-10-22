4b2c81ff5a8c85e8400ab06bdc2412fd818b8d10
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2020-10-22 19:09:10 +0530

    variable

--- decrypt.py
+++ decrypt.py
@@ -1,2 +1,7 @@
 print( "Hello, Themyscira!" )
 # This is a comment that won't be interpreted as a command.
+# Associate variable diana with the value "WONDER WOMAN 1984"
+diana = "WONDER WOMAN 1984"
+
+# Print a message with the true identity of Diana
+print( "I believe Diana is actually " + diana )
